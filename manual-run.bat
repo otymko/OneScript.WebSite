ca61b1ac03e2dc5de75277d868b44433d22bf1f2
Oleg Tymko <olegtymko@yandex.ru>
2019-04-19 14:11:00 +0700

    Приоритет переменных среды над конфигурацией в json. Пример запуска через cmd с переменными среды

--- manual-run.bat
+++ manual-run.bat
@@ -1,2 +1,4 @@
-cd F:\DATA\Develop\Project\oscript-site\project\src
+cd F:\DATA\Develop\Project\oscript-site\Site\src
+SET OS_CONTENT_DIRECTORY=F:\DATA\Develop\Project\oscript-site\Site\src\content
+SET OS_DOWNLOAD_DIRECTORY=F:\DATA\Develop\Project\oscript-site\Data\downloads
 F:\DATA\Develop\Project\oscript-site\oscriptweb\OneScript.WebHost.exe
